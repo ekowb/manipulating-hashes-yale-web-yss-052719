9706a1572fa7be98dcce36728a8aecb9e78255aa
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-24 17:37:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        attribute.delete_if {|flavor| flavor = "strawberry"}
+        :favorite_icecream_flavors.delete_if {|flavor| flavor = "strawberry"}
         end
       end
     end
